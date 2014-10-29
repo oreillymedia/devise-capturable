fefba2df4ba047bdf6569645464b2a3428ad6731
Marcel M. Cary <marcel@oreilly.com>
2014-10-29 15:10:51 -0700

    Install Devise into spec/rails_app
    We have a baseline test app in spec/rails_app, but it doesn't have
    Devise installed yet, much less Devise::Capturable.
    
    Install Devise into spec/rails_app.  Base the install on Aaron's here,
    which should be pretty vanilla:
    https://github.com/oreillymedia/devise-test/commit/41d3e453697fb4abe3c85a0372dcbbdffcc10628

--- spec/rails_app/config/application.rb
+++ spec/rails_app/config/application.rb
@@ -54,6 +54,9 @@ module Dummy
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Suggested by Devise
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   end
 end
 
