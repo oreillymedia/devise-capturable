c1999b1a4a241a1221d167d8ac16d208a55adb96
Aaron Sumner <asumner@oreilly.com>
2014-11-19 11:07:57 -0600

    WIP: use route helper when checking URL

--- app/helpers/capturable_helper.rb
+++ app/helpers/capturable_helper.rb
@@ -1,6 +1,6 @@
 module CapturableHelper
   def display_logout?(resource_name)
     clean_url = request.original_url.split('?')[0]
-    [new_session_url(resource_name), "#{root_url}/#{resource_name}/reset_password"].exclude? clean_url
+    [new_session_url(resource_name), reset_password_url(resource_name)].exclude? clean_url
   end
 end
