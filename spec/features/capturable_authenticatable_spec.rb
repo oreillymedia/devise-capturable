e3bb7b5f0776ef6c253b4daa0ea3a1c04dd408e7
Zachary Ogren <zogren@oreilly.com>
2014-11-19 13:12:24 -0800

    Adding more complete test coverage of login via the test rails app

---
+++ spec/features/capturable_authenticatable_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "Devise::CapturableAuthenticatable", type: :feature do
+  it "permits logged-out access to index" do
+    visit "/"
+    expect(page).to have_content "Index"
+  end
+
+  it "required login for protected" do
+    visit "/protected"
+    expect(page).not_to have_content "Protected"
+    puts page.html
+    within "h2" do
+      expect(page).to have_content "Log in"
+    end
+    fill_in "Email", :with => test_user_email
+    fill_in 'Password',:with => test_user_password
+    click_button("Log in")
+    expect(page).not_to have_content "Log in"
+    expect(page).to have_content "Protected"
+  end
+end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,19 @@ RSpec.configure do |config|
   #config.infer_base_class_for_anonymous_controllers = false
   config.color = true
 end
+
+def test_user_password
+  load_from_env('TEST_USER_PASSWORD')
+end
+
+def test_user_email
+  load_from_env('TEST_USER_EMAIL')
+end
+
+def load_from_env(variable_name)
+  val = ENV[variable_name]
+  unless val
+    raise "Missing test configuration from environment: #{variable_name}"
+  end
+  val
+end
