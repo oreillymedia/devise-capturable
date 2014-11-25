27179d9a912da049d335031fc2a9f8d54920e8a3
Zachary Ogren <zogren@oreilly.com>
2014-11-24 17:52:06 -0800

    cleanup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < Devise::SessionsController
+
   def create
+    puts "SessionsController.create called"
     self.resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
     sign_in_and_redirect(resource_name, resource)
   end
 
   def sign_in_and_redirect(resource_or_scope, resource=nil)
+    puts "SessionsController.sign_in_and_redirect called"
     scope = Devise::Mapping.find_scope!(resource_or_scope)
     resource ||= resource_or_scope
     sign_in(scope, resource) unless warden.user(scope) == resource
